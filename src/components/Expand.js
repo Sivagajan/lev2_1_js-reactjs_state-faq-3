551c27d4c6680e8d51b665bff813a2f24947a3b6
Sivagajan <sivagajan@gmail.com>
2022-09-01 15:01:25 +0200

    finished

--- src/components/Expand.js
+++ src/components/Expand.js
@@ -4,10 +4,31 @@ import { useState } from "react"
 
 const Expand = () => {
     const [expand, setExpand] = useState(false)
+    const [expand2, setExpand2] = useState(false)
+    const button = document.getElementsByTagName('button')
 
     const goBig = () => {
+
+        if(!expand){
+            button[0].innerHTML = '-'
+        }else{
+            button[0].innerHTML = '+'
+        }
         setExpand(!expand)
+        setExpand2(false)
+        button[1].innerHTML = '+'
     }
+
+    const goBigger = () =>{
+        setExpand2(!expand2)
+
+        if(!expand2){
+            button[1].innerHTML = '-'
+        }else{
+            button[1].innerHTML = '+'
+        }
+    }
+
     return (
         <section>
             <article >
@@ -18,6 +39,12 @@ const Expand = () => {
 
                 <div className="answerDiv" style={!expand?{display:'none'}:null}>
                     <p>Fast Learning Curve</p>
+                    <button onClick={goBigger}> + </button>
+
+                    <div style={!expand2?{display:'none'}:null}>
+                        <p>React is very a simple and lightweight library that only deals with the view layer. It is not a beast like other MV* frameworks such as Angular or Ember. Any Javascript developer can understand the basics and start developing an awesome web application after only a couple of days reading tutorial. As the React guide says ‘Thinking in React’ may be a little different than you used to since it brings a new approach to the table, but it will become much easier and natural as you gain experience with it.
+                        </p>
+                    </div>
                 </div>
             </article>
         </section>
